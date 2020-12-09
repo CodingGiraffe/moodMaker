22fc636edb08412300de8435b2233ad04fa2175c
CodingGiraffe <adamthomas03@gmail.com>
2020-12-09 10:49:10 -0600

    added heroku deployment

--- server/index.js
+++ server/index.js
@@ -4,17 +4,24 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const usersRouter = require('./routers/users');
 const authRouter = require('./routers/auth');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 4001;
+
+const publicPath = path.join(__dirname, '..', 'client/build');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
+app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
   res.send('Welcome to our server!');
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
