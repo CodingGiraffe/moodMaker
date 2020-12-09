293d53c4d5ccfa1d0ed9fcd8a09bbc59d1798d0e
CodingGiraffe <adamthomas03@gmail.com>
2020-12-09 13:52:22 -0600

    changed server files

---
+++ server/build/precache-manifest.66fe9eb4d4d92e9ab1fa450f8000f2b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398aab1837e110b16e061241fb0c083b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f74d5e9f75b65af83d3a",
+    "url": "/static/css/main.9a2dc3eb.chunk.css"
+  },
+  {
+    "revision": "79a4b0fd9642dfc1bcd9",
+    "url": "/static/js/2.711229ca.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/static/js/2.711229ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f74d5e9f75b65af83d3a",
+    "url": "/static/js/main.877ab234.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c39c2da0409074bffaeece5abf4e6097",
+    "url": "/static/media/Happy.c39c2da0.jpg"
+  },
+  {
+    "revision": "bf5bf17f8e9d3d4280cd7c78bc03eae3",
+    "url": "/static/media/Motivated.bf5bf17f.jpg"
+  },
+  {
+    "revision": "966c83fdbd7cb9bca33bdb8dab0a8e3c",
+    "url": "/static/media/WorkoutPic.966c83fd.webp"
+  }
+]);
--- server/index.js
+++ server/index.js
@@ -9,19 +9,23 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 4001;
 
-const publicPath = path.join(__dirname, '..', 'client/build');
+// const publicPath = path.join(__dirname, '..', 'client/build');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
   res.send('Welcome to our server!');
 });
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(publicPath, 'index.html'));
+// });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => {
