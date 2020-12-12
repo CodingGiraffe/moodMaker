be9b63a4d3803b4f2b22ef96479d39e89f144bfc
CodingGiraffe <adamthomas03@gmail.com>
2020-12-11 21:37:13 -0600

    added new build

---
+++ build/precache-manifest.eec907de9073a690dbe44d323adee3f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729c56411e8c37518212772e2525f028",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e485bebaec42a27dd971",
+    "url": "/static/css/main.e6bbe0e3.chunk.css"
+  },
+  {
+    "revision": "6817986d7f16e8a10711",
+    "url": "/static/js/2.ff6d1606.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/static/js/2.ff6d1606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e485bebaec42a27dd971",
+    "url": "/static/js/main.5506d279.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c39c2da0409074bffaeece5abf4e6097",
+    "url": "/static/media/Happy.c39c2da0.jpg"
+  },
+  {
+    "revision": "bf5bf17f8e9d3d4280cd7c78bc03eae3",
+    "url": "/static/media/Motivated.bf5bf17f.jpg"
+  },
+  {
+    "revision": "966c83fdbd7cb9bca33bdb8dab0a8e3c",
+    "url": "/static/media/WorkoutPic.966c83fd.webp"
+  }
+]);
