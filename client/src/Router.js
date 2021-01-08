5f7d3cd04a1772f35b3fc365ec30017b8a244c67
CodingGiraffe <adamthomas03@gmail.com>
2021-01-08 13:19:01 -0600

    debug

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router';
-// import Listing from './containers/Listing'
-// import Listings from './containers/Listings'
 import LoginScreen from './components/LoginScreen';
-// import AddListing from './containers/AddListing'
 import Dashboard from './components/Dashboard';
 import SignUp from './components/SignUp';
 import { connect } from 'react-redux';
@@ -25,13 +22,9 @@ const ProtectedRoute = ({ component: Component, ...rest }) => {
 };
 
 const Router = (props) => {
-  // console.log('props: ', props);
   return (
     <div>
-      {/* {props.loggedIn ? <p>Logged In</p> : <p>Test</p>} */}
       <Switch>
-        {/* <Route exact path="/" component={Listings} /> */}
-        {/* <Route path="/listing/:id" component={Listing}/> */}
         <Route exact path="/" component={LoginScreen} />
         <ProtectedRoute path="/Dashboard" component={Dashboard} />
         <Route exact path="/SignUp" component={SignUp} />
