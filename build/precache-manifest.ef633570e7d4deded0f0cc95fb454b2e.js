0c99a5abb5a301d0ae8ba315a424ef0790b91119
CodingGiraffe <adamthomas03@gmail.com>
2021-01-11 08:56:37 -0600

    added env file to both build folders

---
+++ build/precache-manifest.ef633570e7d4deded0f0cc95fb454b2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "582aa97627f9e18ff792e6475020dffc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b9bc229016301414538",
+    "url": "/static/css/main.e6bbe0e3.chunk.css"
+  },
+  {
+    "revision": "6817986d7f16e8a10711",
+    "url": "/static/js/2.ff6d1606.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/static/js/2.ff6d1606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b9bc229016301414538",
+    "url": "/static/js/main.7d4ed503.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c39c2da0409074bffaeece5abf4e6097",
+    "url": "/static/media/Happy.c39c2da0.jpg"
+  },
+  {
+    "revision": "bf5bf17f8e9d3d4280cd7c78bc03eae3",
+    "url": "/static/media/Motivated.bf5bf17f.jpg"
+  },
+  {
+    "revision": "966c83fdbd7cb9bca33bdb8dab0a8e3c",
+    "url": "/static/media/WorkoutPic.966c83fd.webp"
+  }
+]);
